e681fd77ec6b2ba2f7fcd3ec54d79cabbd9fcd65
Mayur More <100954065+MayurRMore@users.noreply.github.com>
2022-05-10 22:15:43 +0530

    Create Polymorphism.java

---
+++ Polymorphism/Polymorphism.java
@@ -0,0 +1,22 @@
+class aaa
+{
+public void add(int e,int f)
+{
+System.out.println("total is "+(e+f));
+}
+public void add(int e,int f,int g)
+{
+System.out.println("total is "+(e+f+g));
+}
+}
+class psp
+{
+static public void main(String gg[])
+{
+aaa b;
+b=new aaa();
+add(10+100);
+add(10+10+45);
+}
+}
+
